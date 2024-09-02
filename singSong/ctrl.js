e8470af81a95dd0ae7aed55f813614fc63e5b819
choigame3000 <dactandn0@gmail.com>
2024-09-02 12:59:13 +0700

    sing songs

---
+++ singSong/ctrl.js
@@ -0,0 +1,68 @@
+
+var app = angular.module("myApp", ['ngSanitize']);
+app.controller("ctrl", function($scope, $timeout) {
+
+$scope.songs = SONGS;
+
+$scope.songIdx = 0;
+
+$scope.bPlaying = false;
+$scope.bPlayingFull = false;
+$scope.bPause = false;
+
+$scope.audio;
+
+$scope.stopSound = function () {
+	if (!$scope.audio) return;
+	 $scope.audio.pause();
+	 $scope.audio.currentTime = 0;
+	 $scope.currentTime = 0;
+	 $scope.bPlaying = false;
+};
+
+$scope.playAtTime = function (time, songIdx) {
+	console.log('playAtTime', songIdx, time)
+	$scope.stopSound();
+	$scope.audio = undefined;
+
+	var song = $scope.songs[songIdx];
+	$scope.audio = new Audio( song.title + '.mp3' );
+    $scope.audio.loop = true;
+
+	$scope.audio.currentTime = time;
+	$scope.audio.play();
+	$scope.bPlaying = true;
+
+	$scope.audio.addEventListener("ended", function(){
+		$scope.bPlaying = false;
+	});
+
+}
+
+$scope.fetchSong = function (idx) {
+
+}
+
+$scope.loadDB = function (idx) {
+if (localStorage.hasOwnProperty("sing_song_loop")) {
+		document.getElementById('audioLoopEle').checked = localStorage.sing_song_loop === 'true';
+	}
+}
+
+$scope.preProcessSongs = function () {
+	for (var i = 0; i < $scope.songs.length; i++) {
+		var song  = $scope.songs[i]
+		var lyrics  = song.lyrics;
+		for (var j = 0; j < lyrics.length; j++) {
+			var txt = lyrics[j].txt;
+			lyrics[j].txt = txt.replace(/\s*\.\s*/g, "<br>");
+		}
+	}
+}
+
+$scope.loadDB();
+$scope.preProcessSongs();
+
+
+});
+
---
+++ singSong/data.js
@@ -0,0 +1,38 @@
+var SONGS = 
+[
+	{
+		title:"Cheri Cheri Lady",
+		lyrics: 
+		[
+			{txt:"Oh, I cannot explain. Every time it's the same. Oh I feel that it's real, \
+			take my heart. I've been lonely to long. Oh, I can't be so strong.\
+			 Take a chance for romance, take my heart. I need you so. There's no time I'll ever go.", time: 17.7},
+			{txt:"Cheri Cheri Lady. Goin' through a motion. Love is where you find it. Listen to your heart.", time: 41},
+			{txt:"Cheri Cheri Lady.Livin' in devotion.Always like the first time.Let me take a part.Cheri Cheri Lady.\
+			Like there's no tomorrow.Take my heart don't lose it.Listen to your heart.Cheri Cheri Lady.\
+			To know you is to love you.If you call me baby, I'll be always yours.", time: 51.3},
+			{txt:"I get up, I get down.Oh my world turns around.Who is right, who is wrong.I don't know.\
+			I've got pain in my heart.Gotta love in my soul.Easy come but I think easy go.I need you so.All those times I move so slow.", time: 96},
+			{txt:"Cheri Cheri Lady.Goin' through a motion.Love is where you find it.Listen to your heart.Cheri Cheri Lady.Livin' in devotion.Always like the first time.Let me take a part.\
+			Cheri Cheri Lady.Like there's no tomorrow.Take my heart don't lose it.Listen to your heart.\
+			Cheri Cheri Lady.To know you is to love you.If you call me baby, I'll be always yours", time: 120},
+			{txt:"Cheri Cheri Lady.Like there's no tomorrow.Take my heart don't lose it.Listen to your heart.\
+			Cheri Cheri Lady.To know you is to love you.If you call me baby, I'll be yours.", time: 169},
+
+		]
+	},
+	{
+		title:"Sealed with a Kiss",
+		lyrics: 
+		[
+			{txt:"Though we've gotta say goodbye.For the summer.Darling, I promise you this.I'll send you all my love.Every day in a letter.Sealed with a kiss.", time: 10},
+			{txt:"Yes! It's gonna be a cold.Lonely summer.But I'll fill the emptiness.I'll send you all my dreams.Every day in a letter.Sealed with a kiss", time: 31.5},
+			{txt:"I'll see you.In the sunlight.I'll hear your voice everywhere.I'll run.To tenderly hold you.But, darling, you won't be there!\
+			.I don't want to say goodbye.For the summer.Knowing the love we'll miss.Oh! Let us make a pledge.To meet in September.And seal it with a kiss", time: 53},
+			{txt:"Yes! It's gonna be a cold.Lonely summer.But I'll fill the emptiness.I'll send you all my love.\
+			Every day in a letter.Sealed with a kiss.Sealed with a kiss.Sealed with a kiss", time: 113},
+
+		]
+	},
+];
+

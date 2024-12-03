91b5ab4f0fe44ba8404291c8f39d9da4d42e502a
choigame3000 <dactandn0@gmail.com>
2024-12-03 19:29:48 +0700

    f2f

--- ebooks/f2f/f2f_b1_data.js
+++ ebooks/f2f/f2f_b1_data.js
@@ -88,6 +88,108 @@ H Y-O-U-S-E-F<br>\
 K Y-O-U-S-E-E Cảm ơn, Hasan. Chào mừng đến với lớp học.<br>\
 H Cảm ơn bạn."
 	,voca:"",
+},
+{
+	track:"02.06",
+	en:"AMY Ben, do you want to see some photos of my friends?<br>\
+BEN Sure.<br>\
+A OK. This is a friend from Germany.<br>\
+B What's his name?<br>\
+A Karl. He's a doctor.<br>\
+B Oh, OK.<br>\
+A And, um, this is my friend Steve. He's a musician.<br>\
+B Is he American?<br>\
+A Yes, he is. He's from Washington. And, er, this is my friend Claire.<br>\
+B That's a nice photo. Where's she from?<br>\
+A She's from France. But she's a teacher in London now.<br>\
+B Really? Hmm. She's beautiful.<br>\
+A Yes, she is and she's married.<br>\
+B Oh.<br>\
+A And this is my friend Daniela. She's from Italy.<br>\
+B Is she a musician?<br>\
+A No, she isn't. She's an actress.<br>\
+B Oh, OK. Is she married?<br>\
+A Um, no, she's single.<br>\
+B Really? What's her phone number?<br>\
+A Ben!"
+	,vi:"AMY Ben, bạn có muốn xem một số bức ảnh của bạn bè tôi không?<br>\
+BEN Chắc chắn rồi.<br>\
+A OK. Đây là một người bạn đến từ Đức.<br>\
+B Tên anh ấy là gì?<br>\
+A Karl. Anh ấy là bác sĩ.<br>\
+B Ồ, OK.<br>\
+A Và, ừm, đây là bạn tôi Steve. Anh ấy là nhạc sĩ.<br>\
+B Anh ấy có phải người Mỹ không?<br>\
+A Vâng, anh ấy là người Mỹ. Anh ấy đến từ Washington. Và, ừm, đây là bạn tôi Claire.<br>\
+B Bức ảnh đẹp đấy. Cô ấy đến từ đâu?<br>\
+A Cô ấy đến từ Pháp. Nhưng hiện cô ấy là giáo viên ở London.<br>\
+B Thật sao? Ừm. Cô ấy xinh đẹp.<br>\
+A Vâng, cô ấy là người Mỹ và cô ấy đã kết hôn.<br>\
+B Ồ.<br>\
+A Và đây là bạn tôi Daniela. Cô ấy đến từ Ý.<br>\
+B Cô ấy có phải là nhạc sĩ không?<br>\
+A Không, cô ấy không phải. Cô ấy là diễn viên.<br>\
+B Ồ, được rồi. Cô ấy đã kết hôn chưa?<br>\
+A Ừm, không, cô ấy còn độc thân.<br>\
+B Thật sao? Số điện thoại của cô ấy là gì?<br>\
+A Ben!"
+	,voca:"",
+},
+{
+	track:"02.13",
+	en:"TONY Good morning. Welcome to the nine2five Employment Agency. My name's Tony.<br>\
+AMY Nice to meet you.<br>\
+T You too. OK. First, one or two questions. What's your first name, please?<br>\
+A It's Amy.<br>\
+T And what's your surname?<br>\
+A Foley.<br>\
+T OK, um, how do you spell that, please?<br>\
+A F-O-L-E-Y.<br>\
+T F-O-L-E-Y. Thanks. Are you married? A No, I'm single.<br>\
+A nd what's your nationality?<br>\
+A I'm British.<br>\
+T OK ...Right. What's your address?<br>\
+A It's 9 Whedon Road, Manchester, M11 6JZ.<br>\
+T How do you spell 'Whedon'?<br>\
+A W-H-E-D-O-N.<br>\
+T So that's 9 Whedon Road, Manchester, M11 6JZ.<br>\
+A Yes, that's right.<br>\
+T OK, thanks. Right, er, next question. What's your mobile number?<br>\
+A It's 07866 642339.<br>\
+T 07866... er...<br>\
+A 642339.<br>\
+T OK, thanks. And what's your email address?<br>\
+A It's amy.foley@hotmail.co.uk. <br>\
+T So that's, er, amy.foley@hotmail.co.uk.<br>\
+A Yes, that's right.<br>\
+T OK, thanks a lot. Right, um, what kind of job do you want?"
+	,vi:"TONY Chào buổi sáng. Chào mừng đến với Cơ quan việc làm nine2five. Tôi tên là Tony.<br>\
+AMY Rất vui được gặp bạn.<br>\
+T Bạn cũng vậy. Được rồi. Trước tiên, một hoặc hai câu hỏi. Tên của bạn là gì, làm ơn?<br>\
+A Tôi là Amy.<br>\
+T Họ của bạn là gì?<br>\
+A Foley.<br>\
+T Được rồi, ừm, bạn đánh vần từ đó như thế nào, làm ơn?<br>\
+A F-O-L-E-Y.<br>\
+T F-O-L-E-Y. Cảm ơn. Bạn đã kết hôn chưa? A Không, tôi còn độc thân.<br>\
+Và quốc tịch của bạn là gì?<br>\
+A Tôi là người Anh.<br>\
+T Được rồi ...Đúng rồi. Địa chỉ của bạn là gì?<br>\
+A Là số 9 Whedon Road, Manchester, M11 6JZ.<br>\
+T Bạn đánh vần 'Whedon' như thế nào?<br>\
+A W-H-E-D-O-N.<br>\
+T Vậy là số 9 Whedon Road, Manchester, M11 6JZ.<br>\
+A Vâng, đúng rồi.<br>\
+T Được rồi, cảm ơn. Đúng rồi, ừm, câu hỏi tiếp theo. Số điện thoại di động của bạn là gì?<br>\
+A Là số 07866 642339.<br>\
+T 07866... ​​ừm...<br>\
+A 642339.<br>\
+T Được rồi, cảm ơn. Và địa chỉ email của bạn là gì?<br>\
+A Là amy.foley@hotmail.co.uk. <br>\
+T Vậy thì, ừm, amy.foley@hotmail.co.uk.<br>\
+A Vâng, đúng rồi.<br>\
+T OK, cảm ơn rất nhiều. Đúng rồi, ừm, bạn muốn làm công việc gì?"
+	,voca:"",
 }
 
 

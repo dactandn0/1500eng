6a69f8cd9ced3edb240b636f766a38b204218c88
choigame3000 <dactandn0@gmail.com>
2024-12-13 02:38:33 +0700

    tfl

--- ebooks/tfl/tfl_b1_data.js
+++ ebooks/tfl/tfl_b1_data.js
@@ -1,7 +1,6 @@
 
 const tfl_b1_stories = [
 {
-	unit: 1,
 	title:" Names and Titles",
 	track:"01.02",
 	en:"1<br>\
@@ -9,71 +8,70 @@ A My name is Maria Carter and I have a reservation.<br>\
 B Is that M-A-R-Y C-A-R-T-E-R?<br>\
 A No, Maria. M-A-R-I-A.<br>\
 B Oh, right. Here it is. Two nights, right?<br>\
-A That’s right.<br>\
+A That's right.<br>\
 2<br>\
 A Can I have your name, please?<br>\
 B Smith. Suzanna Smith.<br>\
 A Is that S-U-S-A-N?<br>\
 B No, S-U-Z-A-N-N-A.<br>\
-A Sorry, Ms. Smith. I don’t see anything here. And the hotel is full tonight.<br>\
+A Sorry, Ms. Smith. I don't see anything here. And the hotel is full tonight.<br>\
 B What?<br>\
 3<br>\
 A Mr. Wilson?<br>\
-B That’s right. Harry Wilson.<br>\
+B That's right. Harry Wilson.<br>\
 A Is that H-A-R-V-E-Y?<br>\
 B No, H-A-R-R-Y. They call me Dirty Harry!<br>\
 A Oh.<br>\
 4<br>\
-A My name’s Abramson. A-B-R-A-M-S-O-N. Do you have a reservation for me?<br>\
+A My name's Abramson. A-B-R-A-M-S-O-N. Do you have a reservation for me?<br>\
 B Hmm… just a minute. First name Joseph?<br>\
-A That’s right. Joseph.<br>\
+A That's right. Joseph.<br>\
 B Ah, yes. Here it is.<br>\
 5<br>\
-A My name’s Louis Jackson.<br>\
+A My name's Louis Jackson.<br>\
 B Is that L-O-U-I-S-E?<br>\
-A No, that’s a girl’s name.<br>\
+A No, that's a girl's name.<br>\
 B Oh, sorry.<br>\
-A It’s L-O-U-I-S.<br>\
+A It's L-O-U-I-S.<br>\
 B Of course.<br>\
 6<br>\
 A Can I have your name, please?<br>\
 B Cruise. C-R-U-I-S-E.<br>\
 A And your first name?<br>\
-B Marley. That’s M-A-R-L-E-Y.<br>\
-A That’s an unusual name.<br>\
+B Marley. That's M-A-R-L-E-Y.<br>\
+A That's an unusual name.<br>\
 B Yes, it is. I hate it!"
 	,vi:""
 	,voca:"",
 },
 {
-	unit: 1,
 	title:"Names and Titles",
 	track:"01.03",
 	en:"1<br>\
 A Hello.<br>\
 B Hello. Can I speak to Cindy, please?<br>\
-A Sorry. She’s not in. Can I take a message?<br>\
+A Sorry. She's not in. Can I take a message?<br>\
 B Yes, this is Bob from school. Bob Jackson.<br>\
 A Can you spell your last name?<br>\
 B J-A-C-K-S-O-N.<br>\
-A Okay. And what’s your telephone number, Bob?<br>\
+A Okay. And what's your telephone number, Bob?<br>\
 B 691-3839.<br>\
 A Okay. Do you want Cindy to call you?<br>\
 B Yes, please.<br>\
-A Fine. I’ll give her the message.<br>\
+A Fine. I'll give her the message.<br>\
 2<br>\
 A Hello. Tom Waite speaking.<br>\
 B Hello. Can I speak to Cindy, please?<br>\
-A Sorry. She’s not here.<br>\
+A Sorry. She's not here.<br>\
 B Can I leave a message?<br>\
 A Yes, of course.<br>\
 B Thanks. This is Nancy. My number is 391-8246. Please ask Cindy to call me.<br>\
-A Okay. I’ll ask her to call you as soon as she gets back.<br>\
+A Okay. I'll ask her to call you as soon as she gets back.<br>\
 B Thank you.<br>\
 3<br>\
-A Hello. This is Cindy’s house. But Cindy’s not home.<br>\
+A Hello. This is Cindy's house. But Cindy's not home.<br>\
 B Oh, I see. Can I leave a message, please?<br>\
-A Yes, I’m ready.<br>\
+A Yes, I'm ready.<br>\
 B Thanks. This is Brian calling. Brian Kennedy.<br>\
 A Is that B-R-I-A-N K-E-N-N-E-D-Y?<br>\
 B Yes, and my number is 271-8914.<br>\
@@ -82,47 +80,329 @@ B Yes, I will.<br>\
 4<br>\
 A Oh no, not another one. Hello.<br>\
 B Hello. Is Cindy there?<br>\
-A She’s not here. Who’s this?<br>\
+A She's not here. Who's this?<br>\
 B Um. This is her teacher, Miss Wilson.<br>\
 A Oh, Miss Wilson. Yes, of course. Do you want to leave your number?<br>\
-B It’s okay. I’ll call back."
+B It's okay. I'll call back."
 	,vi:""
 	,voca:"",
 },
 {
-	unit: 2,
 	title:"Describing People",
 	track:"02.02",
 	en:"1<br>\
 A So is your boss young?<br>\
-B He’s in his thirties, I guess. About 35.<br>\
+B He's in his thirties, I guess. About 35.<br>\
 2<br>\
-A It’s quite long.<br>\
+A It's quite long.<br>\
 B What color is it?<br>\
-A It’s light brown. And it’s a little curly.<br>\
+A It's light brown. And it's a little curly.<br>\
 3<br>\
-A He’s really not very tall, about 175 centimeters.<br>\
-B Oh yeah. That’s not so tall.<br>\
+A He's really not very tall, about 175 centimeters.<br>\
+B Oh yeah. That's not so tall.<br>\
 4<br>\
 A He looks about 17.<br>\
-B No, he’s older than that. He’s almost 25.<br>\
-A No, I don’t believe it. He doesn’t look that old.<br>\
+B No, he's older than that. He's almost 25.<br>\
+A No, I don't believe it. He doesn't look that old.<br>\
 5<br>\
 A She likes to wear it really short.<br>\
 B Yeah? And is it straight or curly?<br>\
-A Curly. Really curly. You can’t miss her when you see her.<br>\
+A Curly. Really curly. You can't miss her when you see her.<br>\
 6<br>\
 A Is she short?<br>\
-B No, she’s really tall. About 180 centimeters.<br>\
+B No, she's really tall. About 180 centimeters.<br>\
 7<br>\
 A Is she in her teens or her twenties?<br>\
-B I think she’s in her twenties. She’s really nice. Do you want to meet her?<br>\
+B I think she's in her twenties. She's really nice. Do you want to meet her?<br>\
 A Yeah, sure.<br>\
 8<br>\
-A It’s not really long but it’s very straight. And it’s sometimes green!<br>\
+A It's not really long but it's very straight. And it's sometimes green!<br>\
 B Green!<br>\
 A Yeah. He sings in a rock band, I think."
 	,vi:""
 	,voca:"",
+},
+{
+	track:"02.03",
+	en:"1<br>\
+What does your girlfriend look like, Tony?<br>\
+Cindy? Oh, she's tall. And she's got long, dark brown hair.<br>\
+2<br>\
+Tell me about your boyfriend, Anne.<br>\
+Well, his name's Bob. He's 17. Let me see... Well, he's got curly blond hair. He's not very tall—about average. But he's really good-looking.<br>\
+3<br>\
+So, Bob, what's the new girl in class like?<br>\
+She's pretty tall, about 170 centimeters. She's got glasses and short curly hair. I think she's about 20.<br>\
+What's her name?<br>\
+I can't remember. Anne, I think.<br>\
+4<br>\
+So tell me about your cousin, Paul.<br>\
+Well, she's very pretty.<br>\
+Really! Is she blond?<br>\
+No, she's got dark brown hair. Everybody likes her. She's an actress.<br>\
+Really? I'd like to meet her."
+	,vi:""
+	,voca:"",
+},
+{
+	track:"02.04",
+	en:"1<br>\
+It's my little boy! We were looking for some pants for my husband. Now I can't find him.<br>\
+Don't worry, ma'am. We'll find him. How old is he?<br>\
+He's seven.<br>\
+I see. And what color is his hair?<br>\
+It's light brown.<br>\
+Don't worry. We'll find him for you.<br>\
+2<br>\
+My little girl was here a minute ago, and now I can't find her.<br>\
+She's probably in the toy section. Can you describe her?<br>\
+Yes, she's five years old.<br>\
+And what color hair does she have?<br>\
+Brown. And it's very curly.<br>\
+All right. Let's go to the toy section and see if she's there.<br>\
+3<br>\
+Has anyone seen a young kid?<br>\
+How old, sir?<br>\
+He's ten. He's always getting lost. Drives me crazy.<br>\
+Where did you last see him?<br>\
+Over by the video equipment.<br>\
+What color is his hair?<br>\
+Blond, and pretty long. Too long!<br>\
+4<br>\
+Excuse me. I've lost my little boy. We were looking at some microwaves and he was bored and…<br>\
+Calm down, sir. Can you describe him?<br>\
+Yes. He's eight. Really cute.<br>\
+And what color is his hair?<br>\
+Dark brown.<br>\
+Okay. Let's see where he is. Just sit down and relax.<br>\
+5<br>\
+Excuse me. I've lost my daughter!<br>\
+Is she around 13, about average height, with short blond hair?<br>\
+Why, yes.<br>\
+She's looking for you—over there, in the shoe department.<br>\
+Thanks a lot!"
+	,vi:""
+	,voca:"",
+},
+{
+	title:"Clothes",
+	track:"03.02",
+	en:"1<br>\
+Which one is David?<br>\
+David? There he is. He's wearing a jacket and tie. And he's got reddish-blond hair. Okay. Thanks.<br>\
+2<br>\
+I'm looking for Janet.<br>\
+Oh, she's wearing a long dress. And she's got glasses.<br>\
+Does she have long hair?<br>\
+No, it's not really long.<br>\
+3<br>\
+Which one is Ron?<br>\
+He's wearing a dark suit and a tie... and sneakers.<br>\
+Sneakers? Okay. Thanks.<br>\
+4<br>\
+I'm looking for Barbara.<br>\
+There she is over there. She's wearing white pants and a red blouse.<br>\
+The one with a scarf?<br>\
+Yeah.<br>\
+5<br>\
+Who's Andy?<br>\
+He's wearing shorts and a T-shirt. He's got short blond hair.<br>\
+Oh, I see him. Thanks.<br>\
+6<br>\
+I'm looking for Patty.<br>\
+Mm...  There she is. She's wearing a yellow skirt, a blue blouse, and red sandals.<br>\
+Thanks.<br>\
+7<br>\
+Is Mary here tonight?<br>\
+Yes, she is. See her over there? She's wearing jeans and a green shirt.<br>\
+Jeans and a green shirt.<br>\
+Yes, and she's wearing a funny hat.<br>\
+Where's Ken?<br>\
+8<br>\
+Oh, the bodybuilder. He's here somewhere. He's wearing shorts and a tight shirt, as<br>\
+usual.<br>\
+Tight shirt?<br>\
+Yeah, he likes to show off his muscles!"
+	,vi:"1<br>\
+Ai là David?<br>\
+David? Anh ấy ở đó. Anh ấy mặc áo khoác và cà vạt. Và anh ấy có mái tóc vàng hoe. Được rồi. Cảm ơn.<br>\
+2<br>\
+Tôi đang tìm Janet.<br>\
+Ồ, cô ấy mặc váy dài. Và cô ấy đeo kính.<br>\
+Cô ấy có tóc dài không?<br>\
+Không, tóc không dài lắm.<br>\
+3<br>\
+Ai là Ron?<br>\
+Anh ấy mặc vest tối màu và cà vạt... và giày thể thao.<br>\
+Giày thể thao? Được rồi. Cảm ơn.<br>\
+4<br>\
+Tôi đang tìm Barbara.<br>\
+Cô ấy ở đằng kia. Cô ấy mặc quần trắng và áo cánh màu đỏ.<br>\
+Người có khăn quàng cổ?<br>\
+Ừ.<br>\
+5<br>\
+Andy là ai?<br>\
+Anh ấy mặc quần đùi và áo phông. Anh ấy có mái tóc vàng ngắn.<br>\
+Ồ, tôi thấy anh ấy rồi. Cảm ơn.<br>\
+6<br>\
+Tôi đang tìm Patty.<br>\
+Ừm... Cô ấy kia rồi. Cô ấy mặc váy vàng, áo cánh xanh và dép xăng đan đỏ.<br>\
+Cảm ơn.<br>\
+7<br>\
+Mary có ở đây tối nay không?<br>\
+Có, cô ấy ở đây. Bạn có thấy cô ấy ở đằng kia không? Cô ấy mặc quần jean và áo sơ mi xanh lá cây.<br>\
+Quần jean và áo sơ mi xanh lá cây.<br>\
+Vâng, và cô ấy đội một chiếc mũ buồn cười.<br>\
+Ken đâu rồi?<br>\
+8<br>\
+Ồ, anh chàng thể hình. Anh ấy ở đâu đó quanh đây. Anh ấy mặc quần short và áo bó, như<br>\
+thường lệ.<br>\
+Áo bó?<br>\
+Đúng vậy, anh ấy thích khoe cơ bắp!"
+	,voca:"",
+},
+{
+	track:"03.03",
+	en:" How are the pants, sir?<br>\
+ I think they're too short. What do you think?<br>\
+ Yes. I think you're right. They certainly are too short.<br>\
+ Can you get me a longer pair?<br>\
+ Do you like this blouse?<br>\
+ Well, I think it's a little too big.<br>\
+ I guess so. Yes, it is pretty big.<br>\
+ You definitely need a smaller one.<br>\
+ Are the sneakers comfortable?<br>\
+ Well, I think they're a little small for me.<br>\
+ Too small? Let me find a bigger pair.<br>\
+ Are those jeans all right?<br>\
+ I'm afraid they're too big for me.<br>\
+ Okay, then let's find a smaller pair.<br>\
+ How does the T-shirt fit? Is it too tight?<br>\
+ Do you have a looser one?<br>\
+ How do you like the sandals?<br>\
+ Too big, I'm afraid.<br>\
+ Too big? Let me find a smaller size."
+	,vi:"Quần thế nào, thưa ông?<br>\
+Tôi nghĩ là chúng quá ngắn. Ông nghĩ sao?<br>\
+Vâng. Tôi nghĩ là ông đúng. Chúng chắc chắn là quá ngắn.<br>\
+Ông có thể lấy cho tôi một chiếc dài hơn không?<br>\
+Ông có thích chiếc áo cánh này không?<br>\
+Vâng, tôi nghĩ là nó hơi rộng một chút.<br>\
+Tôi đoán là vậy. Vâng, nó khá rộng.<br>\
+Ông chắc chắn cần một chiếc nhỏ hơn.<br>\
+Đôi giày thể thao này có thoải mái không?<br>\
+Vâng, tôi nghĩ là chúng hơi nhỏ đối với tôi.<br>\
+Quá nhỏ? Để tôi tìm một chiếc lớn hơn.<br>\
+Những chiếc quần jean đó có ổn không?<br>\
+Tôi e là chúng quá rộng đối với tôi.<br>\
+Được rồi, vậy thì chúng ta hãy tìm một chiếc nhỏ hơn.<br>\
+Chiếc áo phông này vừa không? Có quá chật không?<br>\
+Bạn có đôi nào rộng hơn không?<br>\
+Bạn thấy đôi dép này thế nào?<br>\
+Tôi sợ là nó quá rộng.<br>\
+Quá rộng? Để tôi tìm đôi nhỏ hơn nhé."
+	,voca:"",
+},
+{
+	track:"03.04",
+	en:"1.<br>\
+Sonia was very well dressed. She had on a jacket and a long, dark blue skirt. She was also wearing a yellow blouse. She wasn’t carrying a bag, but she did have a small wallet with her. Oh, and I remember she had nice gold earrings. I also remember her shoes. She was wearing black shoes with very high heels.<br>\
+2.<br>\
+Brian was very casually dressed. He was wearing some really old jeans—you know, the ones with holes in them—with a new wide leather belt and a white T-shirt. He had on some boots, high brown ones. They looked really nice. He also had a red scarf. He usually wears a couple of big rings, but he wasn’t wearing any at the party. He was wearing an interesting silver watch, though.<br>\
+3.<br>\
+Kevin was wearing a yellow shirt and a green tie. I didn’t like the tie, but I liked the shirt. He also had on brown pants. He was wearing sneakers with long white socks—kind of cute, actually. And he had a leather bag over one shoulder.<br>\
+4.<br>\
+Mrs. Graham looked really good. She had on an interesting long red skirt and a beautiful gold and black jacket. And she had some lovely earrings and a nice necklace. She also had a long pink scarf over one shoulder. She didn’t have a bag. She was wearing sandals."
+	,vi:"1.<br>\
+Sonia ăn mặc rất đẹp. Cô ấy mặc một chiếc áo khoác và một chiếc váy dài màu xanh đậm. Cô ấy cũng mặc một chiếc áo cánh màu vàng. Cô ấy không mang theo túi xách, nhưng cô ấy có một chiếc ví nhỏ bên mình. À, và tôi nhớ cô ấy có đôi bông tai vàng đẹp. Tôi cũng nhớ đôi giày của cô ấy. Cô ấy đi giày đen với gót rất cao.<br>\
+2.<br>\
+Brian ăn mặc rất giản dị. Anh ấy mặc một chiếc quần jean rất cũ—bạn biết đấy, loại có lỗ thủng—với một chiếc thắt lưng da bản rộng mới và một chiếc áo phông trắng. Anh ấy đi một đôi bốt cao màu nâu. Chúng trông thực sự đẹp. Anh ấy cũng có một chiếc khăn quàng cổ màu đỏ. Anh ấy thường đeo một vài chiếc nhẫn lớn, nhưng anh ấy không đeo chiếc nào trong bữa tiệc. Tuy nhiên, anh ấy đeo một chiếc đồng hồ bạc khá thú vị.<br>\
+3.<br>\
+Kevin mặc một chiếc áo sơ mi màu vàng và cà vạt màu xanh lá cây. Tôi không thích chiếc cà vạt, nhưng tôi thích chiếc áo sơ mi. Anh ấy cũng mặc quần nâu. Anh ấy đi giày thể thao với tất dài màu trắng—thực ra thì khá dễ thương. Và anh ấy đeo một chiếc túi da trên một vai.<br>\
+4.<br>\
+Bà Graham trông thực sự ổn. Bà mặc một chiếc váy dài màu đỏ thú vị và một chiếc áo khoác vàng và đen tuyệt đẹp. Bà đeo một số đôi bông tai đáng yêu và một chiếc vòng cổ đẹp. Bà cũng đeo một chiếc khăn dài màu hồng trên một vai. Bà không có túi. Bà đi dép xăng đan."
+	,voca:"",
+},
+{
+	title:"Time",
+	track:"04.02",
+	en:"1<br>\
+Excuse me, what's the time?<br>\
+Uh, it's ten fifteen.<br>\
+Thanks<br>\
+2<br>\
+Do you have the time?<br>\
+Let me see. Yeah. It's ten after two.<br>\
+Two ten? Thanks.<br>\
+3<br>\
+What time is it now?<br>\
+Hey, it's exactly twelve o'clock. Midnight. Happy New Year!<br>\
+4<br>\
+Can you tell me the time, please?<br>\
+Yeah, it's eleven thirty.<br>\
+Wow, I'm late. Thanks.<br>\
+5<br>\
+Do you know what time it is?<br>\
+Yeah. It's a quarter to four.<br>\
+Thanks a lot.<br>\
+6<br>\
+I'm late again. What time is it now?<br>\
+It's seven fifty.<br>\
+Class starts at eight. I've only got ten minutes!"
+	,vi:""
+	,voca:"",
+},
+{
+	track:"04.03",
+	en:"1.<br>\
+You are listening to Star Radio at 90.9 on your FM dial. The station with the best classic rock in town. The time is 7:15. Time for more classic rock...<br>\
+2.<br>\
+This is WBYZ 105.1 FM and the time is coming up to 10:05. And we’ve got more music for you, so stay tuned.<br>\
+3.<br>\
+You are listening to X98.5 on your FM dial—the jazz station. The time now is just a minute away from two o’clock.<br>\
+4.<br>\
+This is your AM station, Talk Radio WXYZ. Time now is 11:02. Don’t touch that dial!<br>\
+5.<br>\
+This is the BBC. The time is six fifteen.<br>\
+6.<br>\
+You are tuned to WQXR, your classical music station. And the time is exactly 1:40. Time for the Ninth Symphony by...<br>\
+7.<br>\
+This is 1010 News Radio. The station with all the news, all the time. Time now is 9:30.<br>\
+8.<br>\
+This is your station for easy listening, KABC. The time right now is going on 5:05."
+	,vi:""
+	,voca:"",
 }
+,{
+	track:"04.04",
+	en:"1.<br>\
+Thank you for calling Cinema World. Our box office opens every afternoon at 2:30. Today in Cinema A, we are showing Holiday in Rome, starring Gina Jones. Show times are 3:40, 5:45, 7:55, and 10:15.<br>\
+2.<br>\
+In Cinema B we are showing Hong Kong Warrior. Show times are 4:30, 6:40, 8:30, and 10:45. Don’t forget to visit our new coffee bar located next to Cinema B. It’s open every day from 2:30<br>\
+to 10:30.<br>\
+3.<br>\
+In Cinema C we are showing Ralph the Rabbit. Show times are 3:00, 4:40, 6:30, and 8:15. Also, at 10 o’clock, tonight only, a sneak preview of Crazy Cops, Hollywood’s latest comedy–action<br>\
+film.<br>\
+4.<br>\
+In Cinema D, we are showing The Alien from Saturn. Show times are 3:20, 5:10, 7:20, and 9:40. Don’t forget our box office closes every evening at 9:55."
+	,vi:""
+	,voca:"",
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ]

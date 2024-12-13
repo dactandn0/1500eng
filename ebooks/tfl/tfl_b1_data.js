a2b324a27f2b7fdcfea26584874cf7f3cc899011
choigame3000 <dactandn0@gmail.com>
2024-12-13 20:18:03 +0700

    tfl

--- ebooks/tfl/tfl_b1_data.js
+++ ebooks/tfl/tfl_b1_data.js
@@ -306,13 +306,13 @@ Quá rộng? Để tôi tìm đôi nhỏ hơn nhé."
 {
 	track:"03.04",
 	en:"1.<br>\
-Sonia was very well dressed. She had on a jacket and a long, dark blue skirt. She was also wearing a yellow blouse. She wasn’t carrying a bag, but she did have a small wallet with her. Oh, and I remember she had nice gold earrings. I also remember her shoes. She was wearing black shoes with very high heels.<br>\
+Sonia was very well dressed. She had on a jacket and a long, dark blue skirt. She was also wearing a yellow blouse. She wasn't carrying a bag, but she did have a small wallet with her. Oh, and I remember she had nice gold earrings. I also remember her shoes. She was wearing black shoes with very high heels.<br>\
 2.<br>\
-Brian was very casually dressed. He was wearing some really old jeans—you know, the ones with holes in them—with a new wide leather belt and a white T-shirt. He had on some boots, high brown ones. They looked really nice. He also had a red scarf. He usually wears a couple of big rings, but he wasn’t wearing any at the party. He was wearing an interesting silver watch, though.<br>\
+Brian was very casually dressed. He was wearing some really old jeans—you know, the ones with holes in them—with a new wide leather belt and a white T-shirt. He had on some boots, high brown ones. They looked really nice. He also had a red scarf. He usually wears a couple of big rings, but he wasn't wearing any at the party. He was wearing an interesting silver watch, though.<br>\
 3.<br>\
-Kevin was wearing a yellow shirt and a green tie. I didn’t like the tie, but I liked the shirt. He also had on brown pants. He was wearing sneakers with long white socks—kind of cute, actually. And he had a leather bag over one shoulder.<br>\
+Kevin was wearing a yellow shirt and a green tie. I didn't like the tie, but I liked the shirt. He also had on brown pants. He was wearing sneakers with long white socks—kind of cute, actually. And he had a leather bag over one shoulder.<br>\
 4.<br>\
-Mrs. Graham looked really good. She had on an interesting long red skirt and a beautiful gold and black jacket. And she had some lovely earrings and a nice necklace. She also had a long pink scarf over one shoulder. She didn’t have a bag. She was wearing sandals."
+Mrs. Graham looked really good. She had on an interesting long red skirt and a beautiful gold and black jacket. And she had some lovely earrings and a nice necklace. She also had a long pink scarf over one shoulder. She didn't have a bag. She was wearing sandals."
 	,vi:"1.<br>\
 Sonia ăn mặc rất đẹp. Cô ấy mặc một chiếc áo khoác và một chiếc váy dài màu xanh đậm. Cô ấy cũng mặc một chiếc áo cánh màu vàng. Cô ấy không mang theo túi xách, nhưng cô ấy có một chiếc ví nhỏ bên mình. À, và tôi nhớ cô ấy có đôi bông tai vàng đẹp. Tôi cũng nhớ đôi giày của cô ấy. Cô ấy đi giày đen với gót rất cao.<br>\
 2.<br>\
@@ -357,11 +357,11 @@ Class starts at eight. I've only got ten minutes!"
 	en:"1.<br>\
 You are listening to Star Radio at 90.9 on your FM dial. The station with the best classic rock in town. The time is 7:15. Time for more classic rock...<br>\
 2.<br>\
-This is WBYZ 105.1 FM and the time is coming up to 10:05. And we’ve got more music for you, so stay tuned.<br>\
+This is WBYZ 105.1 FM and the time is coming up to 10:05. And we've got more music for you, so stay tuned.<br>\
 3.<br>\
-You are listening to X98.5 on your FM dial—the jazz station. The time now is just a minute away from two o’clock.<br>\
+You are listening to X98.5 on your FM dial—the jazz station. The time now is just a minute away from two o'clock.<br>\
 4.<br>\
-This is your AM station, Talk Radio WXYZ. Time now is 11:02. Don’t touch that dial!<br>\
+This is your AM station, Talk Radio WXYZ. Time now is 11:02. Don't touch that dial!<br>\
 5.<br>\
 This is the BBC. The time is six fifteen.<br>\
 6.<br>\
@@ -378,14 +378,253 @@ This is your station for easy listening, KABC. The time right now is going on 5:
 	en:"1.<br>\
 Thank you for calling Cinema World. Our box office opens every afternoon at 2:30. Today in Cinema A, we are showing Holiday in Rome, starring Gina Jones. Show times are 3:40, 5:45, 7:55, and 10:15.<br>\
 2.<br>\
-In Cinema B we are showing Hong Kong Warrior. Show times are 4:30, 6:40, 8:30, and 10:45. Don’t forget to visit our new coffee bar located next to Cinema B. It’s open every day from 2:30<br>\
+In Cinema B we are showing Hong Kong Warrior. Show times are 4:30, 6:40, 8:30, and 10:45. Don't forget to visit our new coffee bar located next to Cinema B. It's open every day from 2:30<br>\
 to 10:30.<br>\
 3.<br>\
-In Cinema C we are showing Ralph the Rabbit. Show times are 3:00, 4:40, 6:30, and 8:15. Also, at 10 o’clock, tonight only, a sneak preview of Crazy Cops, Hollywood’s latest comedy–action<br>\
+In Cinema C we are showing Ralph the Rabbit. Show times are 3:00, 4:40, 6:30, and 8:15. Also, at 10 o'clock, tonight only, a sneak preview of Crazy Cops, Hollywood's latest comedy–action<br>\
 film.<br>\
 4.<br>\
-In Cinema D, we are showing The Alien from Saturn. Show times are 3:20, 5:10, 7:20, and 9:40. Don’t forget our box office closes every evening at 9:55."
+In Cinema D, we are showing The Alien from Saturn. Show times are 3:20, 5:10, 7:20, and 9:40. Don't forget our box office closes every evening at 9:55."
 	,vi:""
+	,voca:"",
+},
+{
+	track:"05.02",
+	en:"1<br>\
+Have you been here long?<br>\
+No, we arrived a few days ago, on the first of the month.<br>\
+And how long are you staying?<br>\
+We're staying exactly two weeks. We leave on the 14th.<br>\
+2<br>\
+When did you get here?<br>\
+I arrived on the 4th.<br>\
+Will you be here long?<br>\
+3<br>\
+Did you just arrive?<br>\
+I arrived on July 2nd.<br>\
+Are you enjoying your vacation?<br>\
+Oh, yes. But it's too short. I leave on the 10th.<br>\
+4<br>\
+When did you arrive?<br>\
+We got in on the 5th.<br>\
+How long are you staying?<br>\
+We're leaving on the 15th. We're just staying ten days.<br>\
+5<br>\
+Did you just arrive?<br>\
+We got here on the 3rd.<br>\
+And do you plan to stay long?<br>\
+Yes, we're not leaving until the end of the month.<br>\
+The 31st?<br>\
+That's right<br>\
+6<br>\
+When did you arrive?<br>\
+On the 6th.<br>\
+And when are you leaving?<br>\
+On the 16th."
+	,vi:""
+	,voca:"",
+},
+{
+	track:"05.03",
+	en:"1.<br>\
+This is Dr. Costello's office. We're calling to change your dental appointment to August 3rd at 9:30 in the morning. Thank you.<br>\
+2.<br>\
+Hi, Don. It's Sue. I'm calling about Cindy's birthday party. It's on July 28th at 8 p.m. Are you free? I'll call you later.<br>\
+3.<br>\
+Hello, Don. This is Aunt Betty. How are you, darling? Listen, I'm coming to town next month and I'd love to see you. I'm arriving on August the tenth at 11:15 in the morning. I'll call you from the airport. Bye!<br>\
+4.<br>\
+Hi, Don. This is Ted. Listen, we can't play tennis on Saturday. Are you free Sunday afternoon, July 26th, around three?<br>\
+5.<br>\
+Hello, Don. This is Francis. I'll be back from my trip on Tuesday, September 22nd. Let's meet in my office that Tuesday around 6 p.m., okay? Let me know.<br>\
+6.<br>\
+This is Star Travel. We've booked your flight to New Orleans for next month. You leave on August 2nd on flight 101 from Kennedy Airport at 2 p.m."
+	,vi:""
+	,voca:"",
+},
+{
+	track:"05.04",
+	en:"1.<br>\
+Isn't it your birthday this month, Ted?<br>\
+Hey! It was a month ago.<br>\
+Oh! Sorry! I forgot all about it. So, did you have a big party or something?<br>\
+No. I had to study because of the exams at school.<br>\
+2<br>\
+Have you had your birthday yet, Jill?<br>\
+No. It's in two months.<br>\
+Oh, that's right. So what are you planning to do this time?<br>\
+I'm going to New York with my mom and dad.<br>\
+Hey! Fabulous!<br>\
+3<br>\
+Have you had your birthday yet, Sue?<br>\
+No. It's not ‘til next month. Don't forget this time!<br>\
+Don't worry. I won't.<br>\
+I'm planning to have a barbecue.<br>\
+Great! That sounds fun!<br>\
+4<br>\
+Is your birthday this month or next month, Brian? I can't remember.<br>\
+Oh, it was a month ago.<br>\
+Really. So, how was it?<br>\
+It was okay, but very quiet. I had a family party at home.<br>\
+Oh, that sounds nice."
+	,vi:""
+	,voca:"",
+},
+{
+	track:"06.02",
+	en:" 1.<br>\
+ I'm a salesperson. I sell computers. I like my job. It's really interesting. And I meet lots of interesting people.<br>\
+ 2.<br>\
+ Yes, I like it a lot. I like acting in front of lots of people. One day I hope to write a book about my work.<br>\
+ 3.<br>\
+ It's an interesting job, and it's good for me because I love travel and flying. I visit lots of countries every year. And well—I know it's silly, but I like my uniform.<br>\
+ 4.<br>\
+ It's a good job for me because I like food and I love cooking. Lots of famous people come to our restaurant, too.<br>\
+ 5.<br>\
+ Nursing is a good job for me. I like helping people—you know, sick people.<br>\
+ 6.<br>\
+ I like my work because I work outside. It's hard work, but the pay's good and it keeps me in shape."
+	,vi:" 1.<br>\
+ I'm a salesperson. I sell computers. I like my job. It's really interesting. And I meet lots of interesting people.<br>\
+ 2.<br>\
+ Yes, I like it a lot. I like acting in front of lots of people. One day I hope to write a book about my work.<br>\
+ 3.<br>\
+ It's an interesting job, and it's good for me because I love travel and flying. I visit lots of countries every year. And well—I know it's silly, but I like my uniform.<br>\
+ 4.<br>\
+ It's a good job for me because I like food and I love cooking. Lots of famous people come to our restaurant, too.<br>\
+ 5.<br>\
+ Nursing is a good job for me. I like helping people—you know, sick people.<br>\
+ 6.<br>\
+ I like my work because I work outside. It's hard work, but the pay's good and it keeps me in shape."
+	,voca:"",
+},
+{
+	track:"06.03",
+	en:"1.<br>\
+Hi, Ted.<br>\
+Oh, hi.<br>\
+Are you still working in the bank?<br>\
+Yeah.<br>\
+2<br>\
+Where are you working now, Sonia?<br>\
+I just started as a chef in an Italian restaurant. Come and try my food.<br>\
+Sounds great—I'd love to!<br>\
+3<br>\
+Are you still at school, Bob?<br>\
+No, I graduated from college last month.<br>\
+Congratulations! And what are you doing now?<br>\
+Nothing much. I'm tired. I'm taking a long vacation.<br>\
+4<br>\
+What do you do, Marie?<br>\
+Me? I'm a lawyer.<br>\
+How long have you been doing that?<br>\
+For about five years. Do you need a good lawyer?<br>\
+Well, not yet!<br>\
+5<br>\
+Are you a receptionist, Suzanne?<br>\
+Yes, I am.<br>\
+Do you like the people you work with?<br>\
+Well, it's my first week on the job, so I'm not sure yet.<br>\
+6<br>\
+Are you still a tennis instructor, Martha?<br>\
+No, I'm not. I didn't like it.<br>\
+So what are you doing now?<br>\
+Well, I'm trying to find a new job.<br>\
+7<br>\
+Do you work in an office, Fred?<br>\
+Not anymore. Now I'm an English teacher. I just had my first class.<br>\
+Really? How was it?<br>\
+It was terrific!<br>\
+8<br>\
+Are you still working at the bookstore, Sue?<br>\
+Yeah, but I don't like it. I have to stand all day and I get so tired.<br>\
+I'm sorry to hear that!"
+	,vi:""
+	,voca:"",
+},
+{
+	track:"06.04",
+	en:"1<br>\
+So how do you like your job, Bill?<br>\
+Well, it was okay at first, but now, after two years, I don't like it.<br>\
+Oh, why's that?<br>\
+It's boring. I do the same thing every day. I'm really sick of it.<br>\
+So why don't you change jobs?<br>\
+I'm well-paid. I like the money!<br>\
+Oh, I see. But you should leave if you're not happy.<br>\
+Yeah, maybe I should.<br>\
+2<br>\
+Do you like teaching children, Christine?<br>\
+Oh, yes! I love working with kids. They're so much fun.<br>\
+Well, I guess you have the perfect job!<br>\
+Yeah, I like it a lot. There's just one thing I don't like.<br>\
+What's that?<br>\
+The distance to school. It's too far away. It takes me an hour to drive there every day.<br>\
+Wow. That must be awful!<br>\
+It is, but the schools that are near me are not as good.<br>\
+3<br>\
+How is your new job going, Anna?<br>\
+Good, thanks. I really like it.<br>\
+What do you like best about it?<br>\
+I think it's the people I work with. They are so nice.<br>\
+People make all the difference in a job, don't they?<br>\
+They sure do. The only trouble is, I have to travel a lot. I'm away from home for about two weeks every month.<br>\
+Yeah, that can be difficult.<br>\
+It is. I hope I won't have to travel so much next year.<br>\
+4<br>\
+Do you enjoy being a salesperson, Nancy?<br>\
+Yes, I do like it. I get to meet so many people.<br>\
+Is it hard work?<br>\
+Yes, it can be. I don't like the long hours. I'm always really tired when I get home at night.<br>\
+That's too bad. Why don't you quit?<br>\
+Because I think my boss is great to work for.<br>\
+5<br>\
+How long have you been working in a restaurant, Martin?<br>\
+For more than five years.<br>\
+Wow. You must really enjoy it.<br>\
+Oh no, I don't enjoy it at all! It's hard work and pretty tiring, too. I'm on my feet all night.<br>\
+Oh, I see.<br>\
+But the tips are great. I really should find a better job soon, though."
+	,vi:"1<br>\
+Vậy Bill, anh thấy công việc của anh thế nào?<br>\
+Ồ, lúc đầu thì ổn, nhưng giờ, sau hai năm, tôi không thích nữa.<br>\
+Ồ, tại sao thế?<br>\
+Chán quá. Tôi làm cùng một việc mỗi ngày. Tôi thực sự phát ngán với nó.<br>\
+Vậy sao anh không đổi việc?<br>\
+Tôi được trả lương cao. Tôi thích tiền!<br>\
+Ồ, tôi hiểu rồi. Nhưng anh nên nghỉ việc nếu anh không vui.<br>\
+Ừ, có lẽ tôi nên nghỉ.<br>\
+2<br>\
+Christine, anh có thích dạy trẻ em không?<br>\
+Ồ, có chứ! Tôi thích làm việc với trẻ em. Chúng rất vui.<br>\
+Ồ, tôi đoán là anh có công việc hoàn hảo!<br>\
+Ồ, tôi rất thích công việc đó. Chỉ có một điều tôi không thích.<br>\
+Cái gì thế?<br>\
+Khoảng cách đến trường. Quá xa. Tôi phải mất một giờ lái xe đến đó mỗi ngày.<br>\
+Ôi trời. Chắc là tệ lắm!<br>\
+Đúng là vậy, nhưng những trường học gần tôi thì không tốt bằng.<br>\
+3<br>\
+Công việc mới của bạn thế nào rồi, Anna?<br>\
+Tốt, cảm ơn bạn. Tôi thực sự thích công việc đó.<br>\
+Bạn thích nhất điều gì ở công việc đó?<br>\
+Tôi nghĩ đó là những người tôi làm việc cùng. Họ rất tốt.<br>\
+Con người tạo nên sự khác biệt trong công việc, đúng không?<br>\
+Họ thực sự tốt. Vấn đề duy nhất là tôi phải đi công tác rất nhiều. Tôi xa nhà khoảng hai tuần mỗi tháng.<br>\
+Đúng vậy, điều đó có thể khó khăn.<br>\
+Đúng vậy. Tôi hy vọng năm sau tôi sẽ không phải đi công tác nhiều như vậy.<br>\
+4<br>\
+Bạn có thích làm nhân viên bán hàng không, Nancy?<br>\
+Có, tôi thích. Tôi được gặp rất nhiều người.<br>\
+Công việc này có vất vả không?<br>\
+Có, có thể. Tôi không thích giờ làm việc dài. Tôi luôn rất mệt mỏi khi về nhà vào ban đêm.<br>\
+Thật tệ. Tại sao bạn không nghỉ việc?<br>\
+Bởi vì tôi nghĩ sếp của tôi là người tuyệt vời để làm việc cùng.<br>\
+5<br>\
+Martin, bạn đã làm việc trong nhà hàng bao lâu rồi?<br>\
+Hơn năm năm rồi.<br>\
+Ồ. Bạn hẳn phải thích lắm.<br>\
+Ồ không, tôi không thích chút nào! Công việc này vất vả và cũng khá mệt mỏi. Tôi phải đứng suốt đêm.<br>\
+Ồ, tôi hiểu rồi.<br>\
+Nhưng tiền boa thì tuyệt. Nhưng thực ra tôi nên sớm tìm một công việc tốt hơn."
 	,voca:"",
 }
 

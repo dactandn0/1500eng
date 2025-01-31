45bc2045a95f55eec2be9e6256be0e901ec22cc8
choigame3000 <dactandn0@gmail.com>
2025-02-01 00:03:33 +0700

    writing

--- ebooks/englab/beginnerCourse/englab_begin_data_W.js
+++ ebooks/englab/beginnerCourse/englab_begin_data_W.js
@@ -5,17 +5,20 @@
 const ENGLAB_BEGIN_DATA_W = [
 {
 	examType:0
+	,unit:1
 	,title:"Việc tập luyện thể thao đầy đặn là quan trọng để có một cơ thể khỏe mạnh và thúc đẩy tuổi thọ."
 	,en:"Regular exercise is instrumental in a healthy body and promotes longevity."
 	,voca:"",
 },{
 	examType:0
+	,unit:1
 	,title:"Sức khỏe là tài sản quí giá của con người và mọi người cần phải bảo vệ nó bằng mọi giá."
 	,en:"Health is a precious human asset and everyone needs to protect it at all costs."
 	,voca:"",
 },
 {
 	examType:0
+	,unit:1
 	,title:"Các quốc gia trên thế giới cần bỏ qua căng thẳng về chính trị để cùng nhau hợp tác để giải quyết các vấn đề toàn cầu như biến đổi khí hậu và an ninh."
 	,en:"Many countries in the world need to ignore/put aside political tensions to cooperate to resolve global issues like climate change and security."
 	,vi:""
@@ -23,101 +26,112 @@ const ENGLAB_BEGIN_DATA_W = [
 	,note:"need do sth|need to do sth"
 },{
 	examType:0
+	,unit:1
 	,title:"Lối sống ít vận động và chế độ ăn không tốt là nguyên nhân chính gây việc gia tăng béo phì ở trẻ em."
 	,en:"A sedentary lifestyle and unbalanced diet are main causes of increasing obesity in children."
 	,vi:""
 	,voca:"A sedentary lifestyle"
 },{
 	examType:0
+	,unit:1
 	,title:"Thời gian chất lượng bên gia đình để chia sẽ và bầu bạn là yếu tố quan trong giúp duy trì sự gắn kết giữa các thành viên."
 	,en:"Quality time with family to share and keep each other's company is an important element to maintain bonds with members."
-	,vi:""
 	,voca:"keep each other's company"
 },{
 	examType:0
+	,unit:1
 	,title:"Sự khai thác tài nguyên thiên nhiên phải được thực hiện một cách bền vững để đảm bảo sự tồn tại của các loài sinh vật và cải thiện chất lượng cuộc sống của con người."
 	,en:"Natural resource exploitation must be conducted sustainably to ensure the survival of creatures and promote the quality of humans' lives."
-	,vi:""
-	,voca:""
 	,note:"THE + N + of + N (luôn có THE)"
 },
 {
-	examType:0,
-	title:"Văn hóa là nét đặc trưng của một quốc gia, nó phản ánh những giá trị và truyền thống lâu đời của dân tộc."
+	examType:0
+	,unit:1
+	,title:"Văn hóa là nét đặc trưng của một quốc gia, nó phản ánh những giá trị và truyền thống lâu đời của dân tộc."
 	,en:"Culture is a characteristic of a country, reflecting values and long-standing traditions of a nation."
-	,voca:""
 	,note:"S+V + [AND/BUT/OR/WHICH...] + S+V"
 },{
-	examType:0,
-	title:"Sự đa dạng văn hóa giữa các quốc gia là điều cần được tôn trọng và đánh giá cao để xây dựng một thế giới hòa bình và phát triển."
+	examType:0
+	,unit:1
+	,title:"Sự đa dạng văn hóa giữa các quốc gia là điều cần được tôn trọng và đánh giá cao để xây dựng một thế giới hòa bình và phát triển."
 	,en:"Cultural diversity among countries needs to be respected and appreciated to build a peaceful and prosperous world."
 	,voca:"",
 },{
-	examType:"0",
-	title:"Du lịch là hoạt động giúp con người khám phá, tìm hiểu và trải nghiệm những nét đẹp thiên nhiên và văn hóa địa phương."
+	examType:"0"
+	,unit:1
+	,title:"Du lịch là hoạt động giúp con người khám phá, tìm hiểu và trải nghiệm những nét đẹp thiên nhiên và văn hóa địa phương."
 	,en:"Tourism is an activity for people to explore and expericence the beauty of nature and local culture."
 	,voca:"",
 },
 {
-	examType:"0",
-	title:"Việc đầu tư vào sự theo đuổi về việc học có thể đặt nền móng cho viễn cảnh việc làm tốt hơn và mức sống cao hơn sau này."
+	examType:"0"
+	,unit:1
+	,title:"Việc đầu tư vào sự theo đuổi về việc học có thể đặt nền móng cho viễn cảnh việc làm tốt hơn và mức sống cao hơn sau này."
 	,en:"Investment in academic pursuits can be conducive to better occupational prospects and higher living standards later."
 	,voca:"",
 },
 {
-	examType:"0",
-	title:"Tăng trưởng kinh tế của Việt nam trong năm nay được dự báo sẽ đạt mức 6.5%, cao hơn so với năm ngoái."
+	examType:"0"
+	,unit:2
+	,title:"Tăng trưởng kinh tế của Việt nam trong năm nay được dự báo sẽ đạt mức 6.5%, cao hơn so với năm ngoái."
 	,en:"The Vietnamese economic growth this year is predicted to reach 6.5%, higher than last year."
 	,voca:""
 },{
-	examType:"0",
-	title:"Chính phủ đang tăng cường đầu tư vào cơ sở hạ tầng để nâng cao năng lực cạnh tranh của đất nước."
+	examType:"0"
+	,unit:2
+	,title:"Chính phủ đang tăng cường đầu tư vào cơ sở hạ tầng để nâng cao năng lực cạnh tranh của đất nước."
 	,en:"The government is strengthening/ boosting/ enhancing investment in infrastructure to improve the country’s competitiveness."
-	,voca:""
 	,note:"strengthen không được tự nhiên lắm|khi nói danh từ khái niệm chung thì không dùng the (infrastructure)"
 },{
-	examType:"0",
-	title:"Tỷ giá ngoại tệ giữ đà ổn định trong những tuần qua, tạo điều kiện thuận lợi cho hoạt động xuất nhập khẩu của doanh nghiệp."
+	examType:"0"
+	,unit:2
+	,title:"Tỷ giá ngoại tệ giữ đà ổn định trong những tuần qua, tạo điều kiện thuận lợi cho hoạt động xuất nhập khẩu của doanh nghiệp."
 	,en:"The foreign exchange rate has remained stable in the past weeks, facilitating the import-export activities of businesses."
-	,voca:""
 	,note:"Dùng HTHT vì diễn tả việc bắt đầu trong QK, kéo dài đến hiện tại và kết quả có thể thấy được ở hiện tại|Last week -> tuần vừa rồi -> các tuần vừa qua : in the past weeks|facilitate + Noun: tạo điều kiện -> vốn dĩ tạo điều kiện đã mang nghĩa tích cực rồi nên không cần thiết thêm favourably"
 },{
-	examType:"0",
-	title:"Sự gia tăng lãi xuất của ngân hàng trung ương có thể gây ảnh hưởng đến hoạt động kinh doanh của các doanh nghiệp vay vốn."
+	examType:"0"
+	,unit:2
+	,title:"Sự gia tăng lãi xuất của ngân hàng trung ương có thể gây ảnh hưởng đến hoạt động kinh doanh của các doanh nghiệp vay vốn."
 	,en:"Increasing interest rates of the central bank could affect the activities of borrowing enterprises."
 	,voca:""
 },{
-	examType:"0",
-	title:"Giá dầu thô trên thị trường thế giới đang có xu hướng giảm, đây là cơ hội để các quốc gia nhập khẩu năng lượng tiết kiệm chi phí."
+	examType:"0"
+	,unit:2
+	,title:"Giá dầu thô trên thị trường thế giới đang có xu hướng giảm, đây là cơ hội để các quốc gia nhập khẩu năng lượng tiết kiệm chi phí."
 	,en:"The price of crude oil in the world market is decreasing, which is a chance for energy-importing countries to save costs"
 	,voca:"crude oil"
-	,note:"S+V, which +S+V"
+	,note:"S+V, which/and/so... +S+V"
 },{
-	examType:"0",
-	title:"Chính phủ cần đưa ra những chính sách thúc đẩy phát triển năng lượng sạch, giảm ô nhiễm môi trường và đảm bảo an ninh năng lượng cho đất nước."
+	examType:"0"
+	,unit:2
+	,title:"Chính phủ cần đưa ra những chính sách thúc đẩy phát triển năng lượng sạch, giảm ô nhiễm môi trường và đảm bảo an ninh năng lượng cho đất nước."
 	,en:"The government needs to introduce policies to promote clean energy development, reduce environmental pollution and ensure energy stability."
 	,voca:"clean energy development"
 	,note:"clean energy development, dùng cụm danh từ sẽ trang trọng hơn"
 },{
-	examType:"0",
-	title:"Tình hình đối ngoại của Việt nam đang được cải thiện, điều này có thể giúp tăng cường quan hệ hợp tác kinh tế với các quốc gia khác."
+	examType:"0"
+	,unit:2
+	,title:"Tình hình đối ngoại của Việt nam đang được cải thiện, điều này có thể giúp tăng cường quan hệ hợp tác kinh tế với các quốc gia khác."
 	,en:"Foreign relations in Vietnam have been improving, which can help to strengthen economic cooperation with other countries."
 	,voca:"",
 },{
-	examType:"0",
-	title:"Việt nam cần nâng cao năng lực cạnh tranh của mình để thu hút đầu tư từ các nhà đầu tư nước ngoài và phát triển nền kinh tế bền vững."
+	examType:"0"
+	,unit:2
+	,title:"Việt nam cần nâng cao năng lực cạnh tranh của mình để thu hút đầu tư từ các nhà đầu tư nước ngoài và phát triển nền kinh tế bền vững."
 	,en:"Vietnam needs to improve its competitiveness to attract the investment from foreign investors and develop a sustainable economy."
 	,voca:""
 	,note:"Thêm its vào sẽ giúp bài có tính liên kết hơn"
 },{
-	examType:"0",
-	title:"Điều kiện thuận lợi từ hiệp định thương mại tự do đang giúp Việt nam tăng cường xuất khẩu sang các thị trường khác nhau trên thế giới."
+	examType:"0"
+	,unit:2
+	,title:"Điều kiện thuận lợi từ hiệp định thương mại tự do đang giúp Việt nam tăng cường xuất khẩu sang các thị trường khác nhau trên thế giới."
 	,en:"The favorable conditions from the free trade agreement are helping Vietnam to improve exports to the other different markets in the world."
 	,voca:""
-	,note:"help sb to do sth"
+	,note:"help sb [to] do sth"
 },{
-	examType:"0",
-	title:"Chính sách đầu tư vào giáo dục và đào tạo có vai trò quan trọng trong việc nâng cao chất lượng nguồn nhân lực cho nền kinh tế Việt Nam."
+	examType:"0"
+	,unit:2
+	,title:"Chính sách đầu tư vào giáo dục và đào tạo có vai trò quan trọng trong việc nâng cao chất lượng nguồn nhân lực cho nền kinh tế Việt Nam."
 	,en:"Investment policy in education and training plays a crucial role in for improving the quality of human resources in the Vietnam economy."
 	,voca:""
 	,note:""
@@ -128,83 +142,132 @@ const ENGLAB_BEGIN_DATA_W = [
 	,voca:"eating habits, concerning,sweets,lean meats, physical activity, the accumulation of, weight gain"
 	,note:"Cause (v,n), có thể dùng a cause of + Noun|Cause sb to do sth|Nếu muốn đề cập đến hành động/chủ đề vừa thảo luận phía trước thì dùng this (leads)"
 },{
-	examType:"0",
-	title:"Chính phủ cần tăng cường đầu tư vào giáo dục để nâng cao chất lượng cuộc sống của người dân."
+	examType:"0"
+	,unit:3
+	,title:"Chính phủ cần tăng cường đầu tư vào giáo dục để nâng cao chất lượng cuộc sống của người dân."
 	,en:"The government needs to improve its investment in education to raise the quality of people's lives."
 	,voca:""
 	,note:"lives = phural of life"
 },{
-	examType:"0",
-	title:"Trách nhiệm của mỗi công dân là giữ gìn văn hóa truyền thống của quê hương."
+	examType:"0"
+	,unit:3
+	,title:"Trách nhiệm của mỗi công dân là giữ gìn văn hóa truyền thống của quê hương."
 	,en:"The responsibility of each citizen is to preserve the traditional cuture of their homeland."
 	,voca:"responsibility"
 	,note:"responsibility = obligation"
 },{
-	examType:"0",
-	title:"Tình trạng thất nghiệp đang gia tăng, chính phủ cần đưa ra những giải pháp để giải quyết vấn đề này."
+	examType:"0"
+	,unit:3
+	,title:"Tình trạng thất nghiệp đang gia tăng, chính phủ cần đưa ra những giải pháp để giải quyết vấn đề này."
 	,en:"Unemployment is increasing, so the government needs to introduce solutions to solve this problem."
 	,voca:"problem, solutions"
 	,note:"problem = issue, solution = approach"
 },{
-	examType:"0",
-	title:"Du lịch nông thôn đang trở thành xu hướng mới trong ngành du lịch."
+	examType:"0"
+	,unit:3
+	,title:"Du lịch nông thôn đang trở thành xu hướng mới trong ngành du lịch."
 	,en:"Countryside tourism is becoming a new trend in tourism"
 	,voca:"Countryside tourism"
 	,note:""
 },{
-	examType:"0",
-	title:"Các biện pháp phòng chống dịch bệnh đang được triển khai mạnh mẽ nhằm đảm bảo sức khỏe cộng đồng."
+	examType:"0"
+	,unit:3
+	,title:"Các biện pháp phòng chống dịch bệnh đang được triển khai mạnh mẽ nhằm đảm bảo sức khỏe cộng đồng."
 	,en:"Measures to prevent epidemics are rigorously being implemented to ensure public health."
 	,voca:""
 	,note:""
 },{
-	examType:"0",
-	title:"Sự ô nhiễm môi trường ngày càng đáng báo động, đòi hỏi chính phủ phải đưa ra các chính sách và giải pháp để giảm thiểu tác động tiêu cực lên môi trường."
+	examType:"0"
+	,unit:3
+	,title:"Nạn đói đang gây ra những ảnh hưởng nghiêm trọng đến sức khỏe và đời sống của người dân."
+	,en:"Famine is creating/posing/causing ( một số từ cung cấp thêm cho anh) serious effects on the health and life of people."
+	,voca:""
+	,note:"Có 'THE' phía trước health là chung cho health and life rồi nên không cần 'THE' ở life nữa."
+},{
+	examType:"0"
+	,unit:3
+	,title:"Nghiên cứu khoa học đóng vai trọng trong việc phát triển kinh tế và xã hội của mỗi quốc gia."
+	,en:"Science studies play a crucial role in the economic and societal development of each nation."
+	,voca:""
+	,note:""
+},{
+	examType:0
+	,unit:3
+	,title:"Các chương trình chăm sóc sức khỏe tâm thần đang trở nên ngày càng phổ biến và cần thiết."
+	,en:"Mental healthcare programs are becoming popular and necessary."
+	,voca:""
+	,note:"healthcare programs"
+},{
+	examType:0
+	,unit:3
+	,title:"Chính sách giảm giá điện năng lượng tái tạo được đang được áp dụng để thúc đẩy sử dụng nguồn năng lượng sạch và bảo vệ môi trường"
+	,en:"Price reduction policies on renewable energy sources are being applied to promote clean energy usage and protect the environment."
+	,voca:""
+	,note:""
+},{
+	unit:3
+	,title:"Dịch đoạn"
+	,en:"There are several reasons why many people find foreign films more interesting than those produced in their own countries. Firstly, the established film industries in some nations have large budgets for action scenes, special effects and filming in spectacular scenes. Hollywood blockbusters like 'Avatar' or the James Bond films are examples of such productions, and their global appeal is undeniable. Another reason why these big-budget films are so successful is because they often star the most famous actors and actresses, and are produced by the most talented producers and directors. Low-quality and low-budget films made in many countries are therefore often given lower ratings."
+	,voca:""
+	,note:"tránh lặp từ many|tránh lặp từ countries"
+}
+
+,{
+	examType:"0"
+	,unit:4
+	,title:"Sự ô nhiễm môi trường ngày càng đáng báo động, đòi hỏi chính phủ phải đưa ra các chính sách và giải pháp để giảm thiểu tác động tiêu cực lên môi trường."
 	,en:""
 	,voca:""
 	,note:""
 },{
-	examType:"0",
-	title:"Các hoạt động sản xuất và khai thác tài nguyên đang gây ra nhiều tác động xấu đến môi trường, cần có sự quản lý chặt chẽ để giảm thiểu tác động này."
+	examType:"0"
+	,unit:4
+	,title:"Các hoạt động sản xuất và khai thác tài nguyên đang gây ra nhiều tác động xấu đến môi trường, cần có sự quản lý chặt chẽ để giảm thiểu tác động này."
 	,en:"Production and resource exploitation activities are causing various detrimental impacts on the environment, so strict control / management is required to reduce / mitigate / alleviate these effects."
 	,voca:""
 	,note:""
 },{
-	examType:"0",
-	title:"Bố mẹ không nên quá bảo bọc con của họ, việc này có thể khiến trẻ con phát triển thói quen phụ thuộc vào người khác hoặc trở nên hư hỏng."
+	examType:"0"
+	,unit:4
+	,title:"Bố mẹ không nên quá bảo bọc con của họ, việc này có thể khiến trẻ con phát triển thói quen phụ thuộc vào người khác hoặc trở nên hư hỏng."
 	,en:"Parents shouldn’t be overly protective of their children because this can make them develop a habit of depending on others or become spoiled."
 	,voca:""
 	,note:"Quá bảo bọc: be overly protective of| thói quen phục thuộc: a habit of depending on...| trở nên hư hỏng: be/become spoiled/pampered"
 },{
-	examType:"0",
-	title:"Việc quá phụ thuộc vào nguồn nhiên liệu hoá thạch có thể dẫn tới gia tăng lượng khí carbon và nóng lên toàn cầu, cái mà có thể ảnh hưởng tới tất cả sinh vật trên trái đất."
+	examType:"0"
+	,unit:4
+	,title:"Việc quá phụ thuộc vào nguồn nhiên liệu hoá thạch có thể dẫn tới gia tăng lượng khí carbon và nóng lên toàn cầu, cái mà có thể ảnh hưởng tới tất cả sinh vật trên trái đất."
 	,en:""
 	,voca:""
 	,note:"Nhiên liệu hoá thạch: fossil|Carbon: carbon footprints"
 },{
-	examType:"0",
-	title:"Việc có một ngôn ngữ quốc tế có thể giúp mọi người từ nhiều văn hoá khác nhau vượt quá được rào cản ngôn ngữ, từ đó thúc đẩy sự liên kết toàn cầu."
+	examType:"0"
+	,unit:4
+	,title:"Việc có một ngôn ngữ quốc tế có thể giúp mọi người từ nhiều văn hoá khác nhau vượt quá được rào cản ngôn ngữ, từ đó thúc đẩy sự liên kết toàn cầu."
 	,en:""
 	,voca:""
 	,note:"Ngôn ngữ quốc tế: Lingua franca | Rào cản ngôn ngữ: language barriers"
 },{
-	examType:"0",
-	title:"Việc phát triển thói quen đọc sách ở trẻ con có thể tạo điều kiện cho sự phát triển về khả năng sáng tạo thông qua việc tưởng tượng ra các nhân vật khác nhau dựa trên những gì được tả trong sách."
+	examType:"0"
+	,unit:4
+	,title:"Việc phát triển thói quen đọc sách ở trẻ con có thể tạo điều kiện cho sự phát triển về khả năng sáng tạo thông qua việc tưởng tượng ra các nhân vật khác nhau dựa trên những gì được tả trong sách."
 	,en:""
 	,voca:""
 	,note:""
 },{
-	examType:"0",
-	title:"Án phạt nặng hơn như là phạt tiền hoặc phạt tù nên được đưa ra cho những người phạm tội lặp đi lặp lại."
-	,en:""
+	examType:"0"
+	,unit:4
+	,title:"Án phạt nặng hơn như là phạt tiền hoặc phạt tù nên được đưa ra cho những người phạm tội lặp đi lặp lại."
+	,en:"Heavier penalties, such as monetary fines or prison sentences, should be given to repeat criminals."
 	,voca:""
 	,note:""
 },{
-	examType:"0",
-	title:"Tắt đường hay tai nạn giao thông có thể được giải quyết hiệu quả thông qua việc cải thiện cơ sở hạ tầng giao thông như là mở rộng đường, xây thêm cầu vượt, tự ưu hoá hệ thống đèn vs biển báo."
+	examType:"0"
+	,unit:4
+	,title:"Tắt đường hay tai nạn giao thông có thể được giải quyết hiệu quả thông qua việc cải thiện cơ sở hạ tầng giao thông như là mở rộng đường, xây thêm cầu vượt, tự ưu hoá hệ thống đèn vs biển báo."
 	,en:""
 	,voca:""
-	,note:""
+	,note:"traffic infrastructure|bypasses|optimize"
 },{
 	title:"Dịch đoạn"
 	,en:""
@@ -212,20 +275,23 @@ const ENGLAB_BEGIN_DATA_W = [
 	,voca:""
 	,note:""
 },{
-	examType:"0",
-	title:"Việc nghiện game là nguyên nhân chính dẫn tới kết quả học tập kém của nhiều trẻ con ngày nay."
+	examType:"0"
+	,unit:5
+	,title:"Việc nghiện game là nguyên nhân chính dẫn tới kết quả học tập kém của nhiều trẻ con ngày nay."
 	,en:"Game addiction is the main reason that leads to unsatisfactory academic performance of children."
 	,voca:""
 	,note:"reason = factor = culpit | dẫn tới (dùng MĐQH vì ko phải mục đích)"
 },{
-	examType:"0",
-	title:"Việc dành quá nhiều thời gian vào thế giới ảo có thể ảnh hưởng xấu tới mối quan hệ xã hội của mọi người vì mọi người thích nhắn tin hơn là nói chuyện ngoài đời."
+	examType:"0"
+	,unit:5
+	,title:"Việc dành quá nhiều thời gian vào thế giới ảo có thể ảnh hưởng xấu tới mối quan hệ xã hội của mọi người vì mọi người thích nhắn tin hơn là nói chuyện ngoài đời."
 	,en:"Spending excessive time on the virtual world can badly affect people's social relationships because/as they prefer texting to face-to-face communication."
 	,voca:"the virtual world"
 	,note:"relationships (có 's')| affect STH (ko giới từ) | excessive = too much"
 },{
-	examType:"0",
-	title:"Sự đầu tư vào việc học có thể tạo điều kiện cho một viễn cảnh việc làm tốt hơn cho những bạn trẻ."
+	examType:"0"
+	,unit:5
+	,title:"Sự đầu tư vào việc học có thể tạo điều kiện cho một viễn cảnh việc làm tốt hơn cho những bạn trẻ."
 	,en:""
 	,voca:""
 	,note:"academic pursuits|facilitate|occupational prospects"

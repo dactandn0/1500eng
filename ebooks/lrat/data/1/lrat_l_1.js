f23e0ac1369d6cf6718e3f9e5ecdc92b59b66064
choigame3000 <dactandn0@gmail.com>
2025-03-14 20:14:29 +0700

    1

--- ebooks/lrat/data/1/lrat_l_1.js
+++ ebooks/lrat/data/1/lrat_l_1.js
@@ -22,6 +22,7 @@ WOMAN: Oh dear, this seems rather expensive! getmom boo<br>\
 CLERK: Yes ma'am, but you're paying for the best. We're careful and we're fast. Like we say, the only thing we break are speed records getting you moved.<br>\
 WOMAN: Well... maybe that's so... Oh, I nearly forgot to tell you. I don't want my furniture shipped with me. I won't be look- ing for an apartment till after I arrive in America. Would it be possible to put my furniture in storage here for a month, then have it sent along later? <br>\
 CLERK: Of course, we do that all the time. A couple of other things. Here at 'A Moving Experience', we try to pack your things logically. We don't just throw stuff in boxes.<br>\
+<hr>\
 CLERK: Do you have any special requests? You know, things you want packed in some special place, so you know where to find them?<br>\
 WOMAN: Like what?<br>\
 CLERK: Oh, I don't know... Things like dishes maybe. Not to be rude, but you look like a lady who likes to eat.<br>\
@@ -36,8 +37,28 @@ CLERK: Do you want to take special care of it? I mean it's brand new.<br>\
 WOMAN: Take care of it, but nothing special. You can just put it in storage with the furniture.<br>\
 CLERK: That looks like everything we need here. I guess you're all set.<br>\
 WOMAN: That was certainly quick. Thank you, young man. This has been a most moving experience!"
-,en_ans:"1 94635550 |2 Clark House |3 University Drive |4 Monday |5 Thursday |6 a month |7A |8C |9B |10C"
+,en_ans:"1 94635550 |2 Clark House |3 University Drive |4 Monday |5 Thursday |6 a/one month |7A |8C |9B |10C"
+},{
+	track:"1.2"
+	,en:"Good morning everyone, and welcome to the 2nd Annual Wullaballoo Conference on Mastering Computer Languages. I hope you all had a good trip. Before we get underway with today's programme, let me fill you in as to what's on tap for tomorrow, Sunday, February 19th.<br>\
+At 9:00 a.m. right here in the Main Hall, we'll be hearing a lecture from Dr John Smith about 'Computer as Teacher'. Professor Smith, from the University of Melbourne, is a world-class expert in the field of computer-assisted education, and his talk promises to be both stimulating and informative.<br>\
+Immediately afterwards, at 10:30, there will be a presentation of papers by various delegates. That, however, will take place in the Garden Room on the ground floor. If you don't yet know, the Garden Room is also called the Ballroom, and we'll be gathering at the west end, the slightly raised area called Level 2. Just look for the crowd. If you get lost, there are signs in the foyer.<br>\
+After all that thinking, talking, and listening, I expect everyone will be a bit weary. So at 11:15, there will be a break for coffee, cookies, and other light refreshments. These will be available at the aptly named Refreshment Stand, placed by the door back here in the Main Hall. Also, if you choose to skip the formal lunch, you can buy a packed lunch at the stand for a reasonable price.<br>\
+I strongly urge you, however, to join us at the formal lunch. That won't be till one o'clock sharp, so you have time to stroll about town a bit. We'll be eating at the Sea View Restaurant. The restaurant is located right here in the hotel, on the top floor. It's a good dozen flights of stairs, so I suggest you take the lift on the ground floor, eh? If you're not fond of fish, there is an all-you-can-eat barbecue available as well. They even offer wallaby meat!<br>\
+After lunch, we'll troop back downstairs to Level 2 in the Ballroom for the presentation of further papers, which will begin at 2:00 pm. Please try to be on time. I know you'll be a bit tired after lunch, but the Ballroom echoes so with people coming in late. Thank you in advance.<br>\
+Once we've heard the papers, we'll break for afternoon tea at 3:10 p.m. No need to walk. The manager of the refreshment stand has graciously agreed to have tea served in the Ballroom. He's even prom- ised us some special scones, baked from a recipe of his dear old Scottish grandmother.<br>\
+Then, tea being drunk and scones munched, we'll retire here to the Main Hall for some closing remarks and questions. So, by 5:00 we should have the conference wrapped up. But the fun isn't over! This is Australia mates! We'll be flocking to the hotel's own Palm Lounge on the east side of the foyer for an informal reception. You can relax, mingle with the other delegates, and let your hair down a bit. This will run from 5:10 to 6:10, though you're free to stay as long as you like. The lounge manager has informed me that, for the duration of the actual reception, you can have all-you-can-drink beer for $20.00 with purchase of an advance ticket.<br>\
+<hr>\
+And, yes, tickets can be purchased from any conference organiser or at the front desk anytime between now and the start of the reception. I suggest you come by tomorrow evening to pick up the tickets since the conference hall only holds 800 people. That way, you can also get your journey planned ahead of time and be sure not to miss this truly memorable conference. If you want cocktails, however, I'm sorry. You'll have to pay for those at the regular price.<br>\
+Oh my goodness! Speaking of paying, I see I forgot to tell you a couple of things. The first is about lunch. The charge for the lunch will be $15.00 for all you delegates. If you have guests with you, the cost is $25.00 for the general public, and $6.50 for children under the age of 10. That's fifteen dollars each, not total for everyone! Another item is about the lunch menu. I very much urge you to try the fish. I mean, look at the restaurant's name: Sea View. As the name suggests, it is a famous seafood restaurant. The chef is a Basque from Spain, and he really gets quite put out when people ignore his fish specialties for burgers or barbecue. If fish isn't your thing though, try the steak - he makes an exquisite Filet mignon topped with bleu cheese and mushrooms.<br>\
+Finally, if you'd like to buy a ticket, you can have both lunch and the unlimited beer for $35. I should have mentioned that earlier, but I am a bit forgetful. Maybe I should avoid the beer after the conference, eh?<br>\
+Well, I've said my bit. Are there any questions?"
+,en_ans:"Computer as Teacher|University of Melbourne|top floor|ground floor|3:10|Palm Lounge|17C 18B 19B 20A"
 }
+
+
+
+
 ]
 
 

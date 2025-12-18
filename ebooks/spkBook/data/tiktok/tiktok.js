4372d99a3f893015f91a15d0ed1cc75c884339db
choigame <dactandn0@gmail.com>
2025-12-18 20:29:04 +0700

    1

--- ebooks/spkBook/data/tiktok/tiktok.js
+++ ebooks/spkBook/data/tiktok/tiktok.js
@@ -1,32 +1,24 @@
-
-
 const TIKTOK_DATA = [
 {
-	unit:1
-	,track:""
-	,title:"PASSAGE 1"
-	,en:"a. Lifelong learning is especially relevant in today's changing world. <br>\
-b. Whatever choice you make, remember to keep on learning new knowledge and skills.<br>\
-c. Remember that learning is a lifelong journey, so enjoy every moment of it. <br>\
-d. In a few weeks' time, you'll complete your secondary education. <br>\
-e. Some of you may have already made plans for continuing your studies at university or vocational school, while others may wish to start working right away.<br>\
-f. In fact, learning does not stop after graduation because it is a lifelong process."
-	},
-	{
+unit: 1,
+title: "PASSAGE 1",
+en: "a. Lifelong learning is especially relevant in today's changing world.<br>\nb. Whatever choice you make, remember to keep on learning new knowledge and skills.<br>\nc. Remember that learning is a lifelong journey, so enjoy every moment of it.<br>\nd. In a few weeks' time, you'll complete your secondary education.<br>\ne. Some of you may have already made plans for continuing your studies at university or vocational school, while others may wish to start working right away.<br>\nf. In fact, learning does not stop after graduation because it is a lifelong process."
+},
+{
 unit:2
-	,track:""
-	,title:"PASSAGE 2"
-	,en:"a. Besides, doing chores together can actually be fun and it makes the work go faster. <br>\
+,track:""
+,title:"PASSAGE 2"
+,en:"a. Besides, doing chores together can actually be fun and it makes the work go faster. <br>\
 b. Dear Mike, I hope you are well! I've been thinking about why it's important for children to help with housework.<br>\
 c. It's a good way for families to spend time together and learn new skills. <br>\
 d. Firstly, it teaches us responsibility and how to take care of our own things. <br>\
 e. Also, when we help our parents, it shows them respect and gratitude for all they do for us.<br>\
 f. Best wishes."
-	},
+},
 {
-	unit:3,
-	title:"PASSAGE 3",
-	en:"a. Good morning, everyone. As part of our school cultural programme, we need to plan a field trip to a heritage site in Viet Nam. So, where would you like to go?<br>\
+unit:3,
+title:"PASSAGE 3",
+en:"a. Good morning, everyone. As part of our school cultural programme, we need to plan a field trip to a heritage site in Viet Nam. So, where would you like to go?<br>\
 b. What's a mixed heritage site? <br>\
 c. I'd recommend Trang An Scenic Landscape Complex. It’s the only place in Southeast Asia to be recognised as a mixed heritage site by UNESCO.<br>\
 d. Do you have any suggestions for us, Ms Hoa? <br>\
@@ -34,14 +26,42 @@ e. That sounds interesting, but if we want to learn about cultural values of the
 f. It's a site that has both natural and cultural importance. For example, you can go on a boat trip there to enjoy the beautiful landscape. Or you can visit the old temples and monuments to learn about Vietnamese history."
 },
 {
-	unit:4,
-	title:"PASSAGE 4",
-	en:"a. Best wishes. <br>\
+unit:4,
+title:"PASSAGE 4",
+en:"a. Best wishes. <br>\
 b. First of all, self-study gives learners more freedom. They decide what they should study and how to learn depending on their abilities.<br>\
 c. Dear Lucy, I hope you are well. I am thinking about the advantages of self-study - learning at home without a teacher. <br>\
 d. Secondly, they can make learners responsible. They set their learning goals and make study plans to achieve them.<br>\
 e. Finally, it makes them more confident. Completing tasks and solving problems on their own boost learners' confidence."
+},
+{
+unit: 5,
+title: "PASSAGE 5",
+en: "a. First, we should set up some food stalls offering traditional dishes from different cultures.<br>\nb. Let's discuss what activities to include.<br>\nc. That sounds fun! We can call them 'Taste the World'. We can also show visitors how to cook these dishes.<br>\nd. I like your idea, but we don't have any cooking experience. We may need to involve professional cooks.<br>\ne. We've decided to organise a Cultural Diversity Day in our school."
+},
+{
+unit: 6,
+title: "PASSAGE 6",
+en: "a. Last year, I gained valuable experience working part-time at a local computer repair centre, and I also spent six months as a tech support trainee at a major IT firm.<br>\nb. I am writing to apply for the IT support technician position advertised in the local newspaper on Tuesday 5th July.<br>\nc. I am very adaptable, reliable and excellent at multitasking, and I have strong communication skills and natural problem-solving abilities.<br>\nd. I have a bachelor's degree in IT from the University of Leeds, and I have recently completed a master's in IT management.<br>\ne. I have attached a copy of my CV to this letter for your review.<br>\nf. If you have any questions about me, please let me know. I look forward to hearing from you soon."
+},
+{
+unit: 7,
+title: "PASSAGE 7",
+en: "a. Hi Miranda, How are things?<br>\nb. My parents are very strict and set hard rules.<br>\nc. I can't wait to hear from you. Write back soon.<br>\nd. Everything is great at school except I'm having problems with my parents at home.<br>\ne. I really need some advice. Most of the time we get along really well and I enjoy spending time with them.<br>\nf. Hope you are doing well."
+},
+{
+unit: 8,
+title: "PASSAGE 8",
+en: "a. Thank you for considering this request, and I appreciate your efforts in creating memorable experiences for our class.<br>\nb. With the end of the school year approaching, I wanted to inquire about any plans for a year-end party or celebration for our class.<br>\nc. If there are any plans or discussions underway for a year-end party, could you please provide some information regarding the date, time, location, and any arrangements that may be needed.<br>\nd. Additionally, if there's anything the students can contribute or assist with to make the event a success, please let us know.<br>\ne. It would be wonderful to have an opportunity to gather as a class and celebrate our achievements and the bonds we've formed throughout the year.<br>\nf. Many of us students are looking forward to commemorating the end of the academic year and spending some enjoyable time together before the break."
+},
+{
+unit: 9,
+title: "PASSAGE 9",
+en: "a. I heard about this opportunity in a public announcement last Monday.<br>\nb. I am interested in your community development projects because I really care about the life of people in the community and want to offer my services to you.<br>\nc. I am writing to apply for a volunteer position at the local center for community development.<br>\nd. Currently, all my Sunday afternoons are free, and I will be able to help at the center if I am chosen.<br>\ne. I look forward to hearing from you.<br>\nf. I am always available for an interview on any weekday after 4.30 p.m. or at weekends."
+},
+{
+unit: 10,
+title: "PASSAGE 10",
+en: "a. For example, here in Viet Nam, we can enjoy Italian pizza or Japanese sushi, in addition to our traditional food.<br>\nb. On the other hand, Vietnamese specialties, such as pho and banh mi, are also gaining popularity worldwide.<br>\nc. Fast food chains and international restaurants have become popular, offering a wide range of dishes and more food choices to local people.<br>\nd. Moreover, some international dishes use local ingredients, which make them unique and more suitable to local tastes.<br>\ne. In short, you can explore the flavorful world of traditional Vietnamese food or enjoy dishes from other countries around the world in Viet Nam."
 }
-
-
 ]
--- local.bat
+++ local.bat
@@ -1,3 +1,3 @@
-REM python -m SimpleHTTPServer 8000
+ REM python -m SimpleHTTPServer 8000
 
-python -m http.server 8000
+ python -m http.server 8888
